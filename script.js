8b9f9fc800cc645ca6a8e41568f2763743741c13
johmert <johmert@users.noreply.github.com>
2021-11-03 23:39:47 -0700

    random 'purchased method' character stat generation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let display = document.getElementById('character');
+let bought = document.getElementById('character2');
 let rollButton = document.getElementById('roll');
+let buyButton = document.getElementById('buy');
 
 const character = (str, dex, con, int, wis, cha) => {
     return {
@@ -30,8 +32,42 @@ const characterText = character => {
     return result;
 }
 
-const displayCharacter = text => {
+const displayCharacter = () => {
     display.innerHTML = characterText(rolledCharacter());
 }
 
 rollButton.onclick = displayCharacter;
+
+const randomNum = () => {
+    return Math.floor(Math.random() * 7)
+}
+
+const purchaseStatsCharacter = () => {
+    let points = 25;
+    let costs = [8, 9, 10, 11, 12, 13, 14, 15];
+    let stats = [0, 0, 0, 0, 0, 0];
+    let random = randomNum();
+    // go through the stats array
+    // if no value, randomly select a cost
+    // subtract it's placement (in costs) number from "points" (this is the 'purchasing' of the skill)
+    // can only make "purchase" if there are enough points 
+    // (ex: if points = 5, stats[x] cannot equal costs[6] or costs[7])
+    // this randomizes a process suggested in the PHB pg 13
+    while(points > 0){
+        for(let i=0; i<stats.length; i++){
+            if((points - random) >= 0){
+                stats[i] = costs[random];      
+                points -= random;
+            }
+            random = randomNum(); 
+        }
+    }
+    return character(stats[0], stats[1], stats[2], stats[3], stats[4], stats[5]);
+}
+
+
+const boughtCharacter = () => {
+    bought.innerHTML = characterText(purchaseStatsCharacter());
+}
+
+buyButton.onclick = boughtCharacter;
